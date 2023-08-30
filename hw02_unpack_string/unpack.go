361d97b23d7f4580149e170f6b64e9ff98cc34ff
Ivan Skripnikov <greattramak@mail.ru>
2023-08-30 22:28:42 +0300

    HW2 Linter fixes

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,58 +2,54 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"strings"
 	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func getStringSymbol(symbol byte) (str string) {
+	if unicode.IsLetter(rune(symbol)) {
+		str = string(symbol)
+	} else {
+		str = `\n`
+	}
+	return
 }
 
-func main() {
-	s := "d\n5abc"
+func Unpack(s string) (string, error) {
 	var symbolRune rune
 	var builder strings.Builder
 	zeroRune := rune("0"[0])
 	nlRune := rune("\n"[0])
+	lenString := len(s)
 
 	sa := []byte(s)
 
 	for index, symbol := range sa {
 		symbolRune = rune(symbol)
 		if !unicode.IsDigit(symbolRune) && !unicode.IsLetter(symbolRune) && symbolRune != nlRune {
-			fmt.Println("shit1")
+			return "", ErrInvalidString
 		}
 
-		if index == len(s)-1 && symbolRune == zeroRune {
+		if index == lenString-1 && symbolRune == zeroRune {
 			continue
 		}
 
 		if unicode.IsDigit(rune(symbol)) {
 			if index == 0 {
-				fmt.Println("shit2")
-			} else {
-				if unicode.IsDigit(rune(sa[index-1])) {
-					fmt.Println("shit3")
-				}
+				return "", ErrInvalidString
+			} else if unicode.IsDigit(rune(sa[index-1])) {
+				return "", ErrInvalidString
 			}
 		}
 
 		if unicode.IsLetter(rune(symbol)) || rune(symbol) == nlRune {
-			str := ""
-			if unicode.IsLetter(rune(symbol)) {
-				str = string(symbol)
-			} else {
-				str = `\n`
-			}
-			if index < len(s)-1 && rune(sa[index+1]) == zeroRune {
+			str := getStringSymbol(symbol)
+			if index < lenString-1 && rune(sa[index+1]) == zeroRune {
 				continue
 			}
-			if index < len(s)-1 && unicode.IsDigit(rune(sa[index+1])) {
+			if index < lenString-1 && unicode.IsDigit(rune(sa[index+1])) {
 				builder.WriteString(strings.Repeat(str, int(rune(sa[index+1])-'0')))
 			} else {
 				builder.WriteString(str)
@@ -61,5 +57,5 @@ func main() {
 		}
 	}
 
-	fmt.Println(builder.String())
+	return builder.String(), nil
 }
