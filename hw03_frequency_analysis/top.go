e21d61a99c81e0cb8b48d29af484ec6ca941f0a9
Ivan Skripnikov <greattramak@mail.ru>
2023-09-02 14:00:43 +0300

    HW3 Base struct

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,52 @@
-package hw03frequencyanalysis
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
 func Top10(_ string) []string {
 	// Place your code here.
 	return nil
+}
+
+type Word struct {
+	Title string
+	Count int
+}
+
+func getMappedWords(strings []string) map[string]int {
+	resultMap := map[string]int{}
+
+	for _, word := range strings {
+		_, ok := resultMap[word]
+		if ok == false {
+			resultMap[word] = 1
+		} else {
+			resultMap[word]++
+		}
+	}
+
+	return resultMap
+}
+
+func main() {
+	str := `cat and dog, one dog,two cats and one man and one child. Child play with dog and cats. Dogs and cats is a pets`
+	resultMap := getMappedWords(strings.Split(str, ` `))
+
+	resultStruct := []Word{}
+	for key, value := range resultMap {
+		w := Word{
+			Title: key,
+			Count: value,
+		}
+		resultStruct = append(resultStruct, w)
+	}
+
+	sort.Slice(resultStruct, func(i, j int) bool {
+		return resultStruct[i].Count > resultStruct[j].Count
+	})
+
+	fmt.Println(resultStruct[:10])
 }
