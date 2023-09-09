99a9cf0a8c87cb9d13eb119aaeb42e2a528cbc62
Ivan Skripnikov <greattramak@mail.ru>
2023-09-09 22:36:09 +0300

    HW4 Create structure of list interface

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,4 +1,6 @@
-package hw04lrucache
+package main
+
+import "fmt"
 
 type List interface {
 	Len() int
@@ -17,10 +19,102 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	List
+	Count        int
+	FirstElement *ListItem
+	LastElement  *ListItem
+}
+
+func (list list) Len() int {
+	return list.Count
+}
+
+func (list list) Front() *ListItem {
+	return list.FirstElement
+}
+
+func (list list) Back() *ListItem {
+	return list.LastElement
+}
+
+func (list list) PushFront(v interface{}) *ListItem {
+	val := ListItem{Value: v}
+	list.FirstElement = &val
+	list.Count++
+	if list.Count == 0 {
+		list.LastElement = &val
+	}
+
+	return &val
+}
+
+func (list list) PushBack(v interface{}) *ListItem {
+	val := ListItem{Value: v}
+	list.LastElement = &val
+	list.Count++
+	if list.Count == 0 {
+		list.FirstElement = &val
+	}
+
+	return &val
+}
+
+func (list list) Remove(i *ListItem) {
+	currentElement := list.FirstElement
+	for index := 0; index < list.Count; index++ {
+		if i == currentElement {
+			if index == 0 {
+				nextElement := currentElement.Next
+				nextElement.Prev = nil
+				list.FirstElement = nextElement
+			} else if index == list.Count-1 {
+				prevElement := currentElement.Prev
+				prevElement.Next = nil
+				list.LastElement = prevElement
+			} else {
+				nextElement := currentElement.Next
+				prevElement := currentElement.Prev
+				nextElement.Prev = prevElement
+			}
+			list.Count--
+			break
+		} else {
+			currentElement = currentElement.Next
+		}
+	}
+}
+
+func (list list) MoveToFront(i *ListItem) {
+	currentElement := list.FirstElement
+	for index := 0; index < list.Count; index++ {
+		if i == currentElement {
+			if index == 0 {
+
+			} else if index == list.Count-1 {
+				prevElement := currentElement.Prev
+				prevElement.Next = nil
+				list.LastElement = prevElement
+			} else {
+				nextElement := currentElement.Next
+				prevElement := currentElement.Prev
+				nextElement.Prev = prevElement
+			}
+			break
+		} else {
+			currentElement = currentElement.Next
+		}
+	}
+
+	firstElement := list.FirstElement
+	i.Prev = nil
+	i.Next = firstElement
+	list.FirstElement = i
 }
 
 func NewList() List {
 	return new(list)
+}
+
+func main() {
+	fmt.Println("working")
 }
