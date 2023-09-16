c6d87b48adb3dce9f76ef67e21db25896c42205e
Ivan Skripnikov <greattramak@mail.ru>
2023-09-16 19:02:37 +0300

    HW5 add function handling tasks

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -1,7 +1,11 @@
-package hw05parallelexecution
+package main
 
 import (
 	"errors"
+	"fmt"
+	"math/rand"
+	"sync/atomic"
+	"time"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +14,39 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	errorCount := 0
+
+	for i := 0; i < n; i++ {
+		go taskHandler(tasks, &errorCount)
+	}
+	fmt.Println(errorCount)
+	time.Sleep(2 * time.Second)
+
 	return nil
+}
+
+func taskHandler(tasks []Task, errorCount *int) {
+	fmt.Println(tasks)
+	*errorCount++
+}
+
+func main() {
+	tasksCount := 5
+	tasks := make([]Task, 0, tasksCount)
+
+	var runTasksCount int32
+
+	for i := 0; i < tasksCount; i++ {
+		err := fmt.Errorf("error from task %d", i)
+		tasks = append(tasks, func() error {
+			time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+			atomic.AddInt32(&runTasksCount, 1)
+			return err
+		})
+	}
+
+	workersCount := 5
+	maxErrorsCount := 2
+	err := Run(tasks, workersCount, maxErrorsCount)
+	fmt.Println(err)
 }
