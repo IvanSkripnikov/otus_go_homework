78d216e59bfe80922d60425b8ad4eb045cf9627a
Ivan Skripnikov <greattramak@mail.ru>
2023-09-22 16:50:08 +0300

    HW5 review fix 8

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -92,4 +92,76 @@ func TestRun(t *testing.T) {
 		require.LessOrEqual(t, atomic.LoadInt32(&runTasksCount), int32(workersCount+maxErrorsCount),
 			"extra tasks were started")
 	})
+
+	t.Run("tasks with negative maximum errors", func(t *testing.T) {
+		tasksCount := 30
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(150)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
+
+		workersCount := 7
+		maxErrorsCount := -1
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+		require.LessOrEqual(t, atomic.LoadInt32(&runTasksCount), int32(workersCount+maxErrorsCount),
+			"extra tasks were started")
+	})
+
+	t.Run("tasks with random count errors and success tasks", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+		rand.Seed(time.Now().UnixNano())
+
+		var runSuccessCount int32
+		var runErrorCount int32
+		maxErrorsCount := 5
+
+		for i := 0; i < tasksCount; i++ {
+			if i < maxErrorsCount {
+				tasks = append(tasks, func() error {
+					time.Sleep(time.Millisecond * time.Duration(rand.Intn(170)))
+					atomic.AddInt32(&runErrorCount, 1)
+
+					return fmt.Errorf("error from task %d", i)
+				})
+				continue
+			}
+			randNumber := rand.Intn(100) % 2
+
+			if randNumber == 0 {
+				tasks = append(tasks, func() error {
+					time.Sleep(time.Millisecond * time.Duration(rand.Intn(170)))
+					atomic.AddInt32(&runErrorCount, 1)
+
+					return fmt.Errorf("error from task %d", i)
+				})
+			} else {
+				tasks = append(tasks, func() error {
+					time.Sleep(time.Millisecond * time.Duration(rand.Intn(170)))
+					atomic.AddInt32(&runSuccessCount, 1)
+
+					return nil
+				})
+			}
+		}
+
+		workersCount := 10
+
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.True(t, errors.Is(err, ErrErrorsLimitExceeded))
+		require.LessOrEqual(t, atomic.LoadInt32(&runSuccessCount), int32(workersCount+maxErrorsCount),
+			"extra tasks were started")
+		require.LessOrEqual(t, int32(maxErrorsCount), atomic.LoadInt32(&runErrorCount),
+			"extra tasks were started")
+	})
 }
