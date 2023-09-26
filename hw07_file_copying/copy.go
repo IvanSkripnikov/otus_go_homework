f50041bf1f17f06a8611d119982fc250c8e49f76
Ivan Skripnikov <greattramak@mail.ru>
2023-09-26 16:58:24 +0300

    HW7 First version

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 var (
@@ -9,7 +13,61 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+func Copy(fromPath, toPath string, offset, limit int64) error { // мы заранее знаем сколько хотим прочитать
+	// открываем файл
+	file, errOpen := os.Open(fromPath)
+	if errOpen != nil {
+		fmt.Println(errOpen)
+		return nil
+	}
+
+	// проверяем, не превышает ли offset размер файла
+	if isOffsetExceedsFileSize(file, offset) {
+		return ErrOffsetExceedsFileSize
+	}
+
+	b, err := os.ReadFile(fromPath)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(b))
+
+	file, errCreate := os.Create(toPath)
+
+	if errCreate != nil {
+		return errCreate
+	}
+	written, err := file.Write(b)
+	if err != nil {
+		log.Panicf("failed to write: %v", err)
+	}
+	fmt.Println(written)
+	// мы записали 1M данных !
+	defer file.Close() // чтобы очистить буферы ОС
+
 	return nil
+}
+
+func isOffsetExceedsFileSize(file *os.File, offset int64) bool {
+	var readSize int64
+	var readen int64
+	readSize = 1024
+
+	buf := make([]byte, readSize)
+	for readen < readSize {
+		read, err := file.Read(buf[readen:])
+
+		readen += int64(read)
+		if err == io.EOF {
+			// что если не дочитали ?
+			break
+		}
+		if err != nil {
+			log.Panicf("failed to read: %v", err)
+		}
+	}
+	fmt.Println("output", string(buf))
+	return offset > readen
 }
