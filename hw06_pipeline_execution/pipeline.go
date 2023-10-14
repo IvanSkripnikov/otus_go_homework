9a38c6913a24da0a7f0f7b7fbca9ad4177b671c9
Ivan Skripnikov <greattramak@mail.ru>
2023-10-14 14:42:36 +0300

    HW6 Refactoring - add goroutine

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,18 +9,42 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	var out Out
-	for key, stage := range stages {
-		if key == 0 {
-			out = stage(in)
+	var out = in
+
+	// если  пайплайне нет этапов, выходим из функции
+	if len(stages) == 0 {
+		return nil
+	}
+
+	for _, stage := range stages {
+		if done != nil {
+			result := make(Bi)
+			// выполнение функции и переход на следующий этап
+			go execute(out, done, result)
+			out = stage(result)
 		} else {
 			out = stage(out)
 		}
 	}
 
-	if out != nil {
-		return out
-	}
+	return out
+}
 
-	return nil
+func execute(out Out, done In, result Bi) {
+	defer close(result)
+
+	for {
+		select {
+		case <-done:
+			return
+
+		case res := <-out:
+			select {
+			case <-done:
+				return
+
+			case result <- res:
+			}
+		}
+	}
 }
