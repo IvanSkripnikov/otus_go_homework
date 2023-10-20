049b47c93964161efdf51eb3b2bf64d9239f657e
Ivan Skripnikov <greattramak@mail.ru>
2023-10-20 16:56:14 +0300

    HW7 review fix 3

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"errors"
-	"fmt"
-	"io"
-	"log"
-	"math"
-	"os"
-
 	"github.com/cheggaaa/pb/v3"
+	"io"
+	"os"
 )
 
 var (
@@ -17,7 +13,7 @@ var (
 	ErrCreateFile            = errors.New("can't create file")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error { // мы заранее знаем сколько хотим прочитать
+func Copy(fromPath, toPath string, offset, limit int64) error {
 	// открываем файл
 	readFile, errOpen := os.Open(fromPath)
 	if errOpen != nil {
@@ -32,9 +28,11 @@ func Copy(fromPath, toPath string, offset, limit int64) error { // мы зара
 		return ErrOffsetExceedsFileSize
 	}
 
+	if limit > fileSize {
+		limit = fileSize
+	}
+
 	bufferSize := 1024
-	buffer := make([]byte, bufferSize)
-	clearBuffer := make([]byte, bufferSize)
 
 	// Настраиваем прогресс бар
 	progressCounts := getProgressBarLimit(fileSize, offset)
@@ -43,64 +41,51 @@ func Copy(fromPath, toPath string, offset, limit int64) error { // мы зара
 	bar.Set(pb.Bytes, true)
 	defer bar.Finish()
 
+	var (
+		completeHandleCount int64
+		readLen             int64
+		skippedCount        int64
+	)
+
+	if offset > 0 {
+		writeFile, errWrite := os.OpenFile(toPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
+		if errWrite != nil {
+			return ErrCreateFile
+		}
+
+		for skippedCount <= offset {
+			if offset < int64(bufferSize) {
+				readLen = offset
+			} else {
+				readLen = int64(bufferSize)
+			}
+			if _, err := io.CopyN(writeFile, readFile, readLen); err != nil {
+				break
+			}
+
+			skippedCount += readLen
+		}
+
+		writeFile.Close()
+	}
+
 	writeFile, errWrite := os.OpenFile(toPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
 	if errWrite != nil {
 		return ErrCreateFile
 	}
 
-	var writeOffset int64
-	hasEndWrite := false
-
-	for offset < fileSize {
-		read, errRead := readFile.ReadAt(buffer, offset)
-		if errRead != nil && errRead != io.EOF {
-			return errRead
+	for completeHandleCount < limit {
+		if limit < int64(bufferSize) {
+			readLen = limit
+		} else {
+			readLen = int64(bufferSize)
 		}
-
-		switch {
-		// Если прочитали меньше чем ожидалось, то уменьшаем размер буфера
-		case read < bufferSize:
-			buffer = buffer[:read]
-			buffer = buffer[offset : offset+limit]
-			hasEndWrite = true
-
-		// Если заданый лимит меньше чем прочитаная часть данных, то уменьшаем размер буфера
-		case limit > 0 && limit < int64(read):
-			buffer = buffer[:limit]
-			hasEndWrite = true
-
-		// Если текущий проход записи данных, превышает заданый лимит, то уменьшаем размер буфера
-		case limit > 0 && limit < writeOffset+int64(bufferSize):
-			sliceOffset := int64(math.Abs(float64(writeOffset - limit)))
-			buffer = buffer[:sliceOffset]
-			hasEndWrite = true
-		}
-
-		written, errWrite := writeFile.WriteAt(buffer, writeOffset)
-		if errWrite != nil {
-			errRemove := os.Remove(toPath)
-			if errRemove != nil {
-				log.Println(errRemove)
-			}
-
-			errMessage := fmt.Sprintf("error writing to output file, error: %v", errWrite)
-			return errors.New(errMessage)
-		}
-
-		// инкерментим прогрессбар
-		bar.Add(written)
-
-		// очищаем буфер
-		copy(buffer, clearBuffer)
-
-		// перемещаем позицию для следующего чтения в файле
-		offset += int64(read)
-		writeOffset += int64(read)
-
-		// проверяем условия выхода из цикла записи
-		if errRead == io.EOF || hasEndWrite {
+		if _, err := io.CopyN(writeFile, readFile, readLen); err != nil {
 			break
 		}
+
+		completeHandleCount += readLen
+		//fmt.Println(completeHandleCount, limit, fileSize, readLen)
 	}
 
 	// закрываем файл с которыми работали
