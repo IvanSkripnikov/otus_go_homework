d62cd170600da1ca9ab1f9b109f403f1485b9cb2
Ivan Skripnikov <greattramak@mail.ru>
2023-11-06 16:56:12 +0300

    HW9 Fix test 3

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
 	"regexp"
 	"strconv"
@@ -27,7 +28,14 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	builder := strings.Builder{}
+
+	for _, validationErr := range v {
+		errMessage := fmt.Sprintf("%s: %v; ", validationErr.Field, validationErr.Err)
+		builder.WriteString(errMessage)
+	}
+
+	return builder.String()
 }
 
 func Validate(v interface{}) error {
@@ -38,17 +46,18 @@ func Validate(v interface{}) error {
 	}
 
 	var (
-		metaData       []string
-		validateErrors []error
-		check          []string
+		metaData []string
+		check    []string
 	)
 
 	itemValue := reflect.ValueOf(v)
 	numFields := t.NumField()
+	validateErrors := make([]ValidationError, 0, numFields)
 
 	for i := 0; i < numFields; i++ {
 		fieldType := t.Field(i)
 		value := itemValue.Field(i)
+		fieldName := fieldType.Name
 
 		// Проверяем является ли поле публичным
 		if !fieldType.IsExported() {
@@ -67,13 +76,13 @@ func Validate(v interface{}) error {
 			z := getValuesFromReflectValue(value)
 			r := f(z[0], check[1])
 			if r != nil {
-				validateErrors = append(validateErrors, r.(error))
+				validateErrors = append(validateErrors, ValidationError{Field: fieldName, Err: r.(error)})
 			}
 		}
 	}
 
 	if len(validateErrors) > 0 {
-		// return validateErrors[0]
+		return ValidationErrors(validateErrors)
 	}
 
 	return nil
