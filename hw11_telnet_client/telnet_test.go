a0266fd16c5629427c104036ffe0f1b696934ed2
Ivan Skripnikov <greattramak@mail.ru>
2023-11-25 12:52:49 +0300

    HW11 fix test 1

--- hw11_telnet_client/telnet_test.go
+++ hw11_telnet_client/telnet_test.go
@@ -65,14 +65,14 @@ func TestTelnetClient(t *testing.T) {
 	})
 }
 
-func TestGoogleMailServer(t *testing.T) {
+func TestMailServer(t *testing.T) {
 	in := &bytes.Buffer{}
 	out := &bytes.Buffer{}
 
 	timeout, err := time.ParseDuration("10s")
 	require.NoError(t, err)
 
-	address := net.JoinHostPort("smtp.gmail.com", "587")
+	address := net.JoinHostPort("smtp.mail.com", "587")
 	client := NewTelnetClient(address, timeout, io.NopCloser(in), out)
 	require.NoError(t, client.Connect())
 	defer func() { require.NoError(t, client.Close()) }()
@@ -80,19 +80,16 @@ func TestGoogleMailServer(t *testing.T) {
 	err = client.Receive()
 	require.NoError(t, err)
 
-	pattern := "^220 smtp.gmail.com ESMTP (.*) - gsmtp\r\n$"
+	pattern := "^220 mail.com (.*) Nemesis ESMTP Service ready\r\n$"
 	regExp, errRegExp := regexp.Compile(pattern)
 	require.NoError(t, errRegExp)
 
 	responseConnect := out.String()
 	require.Regexp(t, regExp, responseConnect)
 
-	regExp = regexp.MustCompile("220 smtp.gmail.com ESMTP ")
+	regExp = regexp.MustCompile("220 smtp.mail.com ESMTP ")
 	parts := regExp.Split(responseConnect, -1)
-	require.Len(t, parts, 2)
-
-	sessionID := parts[1]
-	require.NotEmpty(t, sessionID)
+	require.Len(t, parts, 1)
 
 	tests := []struct {
 		name     string
@@ -100,20 +97,15 @@ func TestGoogleMailServer(t *testing.T) {
 		expected string
 	}{
 		{
-			name:     "Helo server",
-			message:  "HELO hellogoogle\r\n",
-			expected: "250 smtp.gmail.com at your service\r\n",
+			name:     "Hello server",
+			message:  "HELO hi google\r\n",
+			expected: "250 mail.com Hello hi google \\[.*\\]\r\n",
 		},
 		{
 			name:    "Ehlo server",
-			message: "EHLO smtp.gmail.com\r\n",
+			message: "EHLO mail.com\r\n",
 			//nolint:lll
-			expected: "^250-smtp.gmail.com at your service, \\[.*\\]\r\n250-SIZE 35882577\r\n250-8BITMIME\r\n250-STARTTLS\r\n250-ENHANCEDSTATUSCODES\r\n250-PIPELINING\r\n250-CHUNKING\r\n250 SMTPUTF8\r\n$",
-		},
-		{
-			name:     "Quit connection server",
-			message:  "QUIT\r\n",
-			expected: "221 2.0.0 closing connection " + sessionID,
+			expected: "250-mail.com Hello mail.com \\[.*\\]\r\n250-8BITMIME\r\n250-SIZE 141557760\r\n250 STARTTLS\r\n$",
 		},
 	}
 
@@ -128,7 +120,6 @@ func TestGoogleMailServer(t *testing.T) {
 
 			err = client.Receive()
 			require.NoError(t, err)
-
 			regExp, errRegExp := regexp.Compile(tc.expected)
 			require.NoError(t, errRegExp)
 			require.Regexp(t, regExp, out.String())
